c1a1091c980fbc20b4d4f1609fc021c60ba40edf
Mariane Mukai <maaarimukai@gmail.com>
2022-01-25 14:59:47 -0300

    Add new project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Cálculo do IMC</h1>
+  <h2>Digite seu peso e altura para calcular o IMC</h2>
+  
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" :disabled="imc"/>
+      <label for="input-weight">Peso</label>
+    </span>
+  </div>
+  
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="height" :disabled="imc"/>
+      <label for="input-height">Altura</label>
+    </span>
+  </div>
+ 
+  <div class="div-imc" v-if="!imc">
+    <Button label="Calcular" @click="calculate" class="btn-calculate"/>
+    <Button label="Limpar" @click="clear"></Button>
+  </div>
+
+  <div v-if="imc">
+    <p class="label-result" >Seu IMC é: {{imc}} </p>
+    <p class="label-classification">A classificação do seu IMC é: {{classification}} </p>
+    <Button @click="clear" label="Calcular novamente" ></Button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+      return {
+        height: null,
+        weight: null,
+        imc: null,
+        classification: ""
+      }
+    },
+    methods: {
+      calculate() {
+        this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+        if (this.imc < 18.5) {
+          this.classification = "Magreza";
+        } else if (this.imc >= 18.5 && this.imc < 25) {
+          this.classification = "Normal";
+        } else if (this.imc >= 25 && this.imc < 30) {
+          this.classification = " Sobrepeso";
+        } else if (this.imc >= 30 && this.imc < 40) {
+          this.classification = "Obesidade";
+        } else {
+          this.classification = "Obesidade Grave";
+        }
+      },
+      clear() {
+        this.height = null;
+        this.weight = null;
+        this.imc = null;
+        this.result = null;
+        this.classification = null;
+      }
+    }
 }
 </script>
 
@@ -19,8 +69,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.div-imc {
+  margin-top: 2rem;
+}
+
+.label-result{
+  font-size: 2rem;
+}
+
+.label-classification {
+  font-size: 1.5rem;
+}
+
+.btn-calculate {
+  margin-right: 2rem !important;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import Button from  "primevue/button";
+import InputText from "primevue/inputtext";
+
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component("Button", Button);
+app.component("InputText", InputText)
+
+app.mount('#app')
