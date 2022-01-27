e39ce0e8bad4bb1d284efac41be0bfd1b6c0a966
Mariane Mukai <maaarimukai@gmail.com>
2022-01-27 18:16:40 -0300

    Change style tag

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
 <template>
-  <h1>Cálculo do IMC</h1>
-  <h2>Digite seu peso e altura para calcular o IMC</h2>
+  <h1>BMI Calculator</h1>
+  <div class="main-content">
+    <div class="calculator-section">
+      <div class="div-imc">
+        <span class="p-float-label">
+          <InputText id="input-weight" type="text" v-model="weight" :disabled="imc"/>
+          <label for="input-weight">Weight</label>
+        </span>
+      </div>
+      
+      <div class="div-imc">
+        <span class="p-float-label">
+          <InputMask id="input-height" type="text" v-model="height" mask="9.99" :disabled="imc"/>
+          <label for="input-height">Height</label>
+        </span>
+      </div>
   
-  <div class="div-imc">
-    <span class="p-float-label">
-      <InputText id="input-weight" type="text" v-model="weight" :disabled="imc"/>
-      <label for="input-weight">Peso</label>
-    </span>
-  </div>
-  
-  <div class="div-imc">
-    <span class="p-float-label">
-      <InputText id="input-height" type="text" v-model="height" :disabled="imc"/>
-      <label for="input-height">Altura</label>
-    </span>
-  </div>
- 
-  <div class="div-imc" v-if="!imc">
-    <Button label="Calcular" @click="calculate" class="btn-calculate"/>
-    <Button label="Limpar" @click="clear"></Button>
+      <div class="div-imc" v-if="!imc">
+        <Button label="Calculate" @click="calculate" class="btn-calculate"/>
+        <!-- <Button label="Limpar" @click="clear"></Button> -->
+      </div>
+      <div class="div-imc" v-show="imc">
+        <Button label="Recalculate" @click="clear" class="btn-calculate"/>
+      </div>
+    </div>  
+    <div class="result-section">
+      <p class="p-title"><strong>RESULTS</strong></p>
+      <div class="results" v-if="imc">
+        <p class="p-text">BMI:</p>
+        <p class="label-result">{{imc}} </p>
+      </div>
+      <div class="results" v-if="imc">
+        <p class="p-text">Classification:</p>
+        <p class="label-result">{{classification}} </p>
+      </div>
+    </div>
+    <!-- <div class="result-section" v-if="imc">
+      <p class="label-result" >Seu IMC é: {{imc}} </p>
+      <p class="label-classification">A classificação do seu IMC é: {{classification}} </p>
+      <Button @click="clear" label="Calcular novamente" ></Button>
+    </div> -->
   </div>
 
-  <div v-if="imc">
-    <p class="label-result" >Seu IMC é: {{imc}} </p>
-    <p class="label-classification">A classificação do seu IMC é: {{classification}} </p>
-    <Button @click="clear" label="Calcular novamente" ></Button>
-  </div>
+
 </template>
 
 <script>
@@ -42,15 +59,17 @@ export default {
       calculate() {
         this.imc = (this.weight / (this.height * this.height)).toFixed(2);
         if (this.imc < 18.5) {
-          this.classification = "Magreza";
+          this.classification = "Underweight";
         } else if (this.imc >= 18.5 && this.imc < 25) {
           this.classification = "Normal";
         } else if (this.imc >= 25 && this.imc < 30) {
-          this.classification = " Sobrepeso";
+          this.classification = " Overweight";
         } else if (this.imc >= 30 && this.imc < 40) {
-          this.classification = "Obesidade";
+          this.classification = "Obesity";
+        } else if (this.imc >= 40){
+          this.classification = "Severe Obesity";
         } else {
-          this.classification = "Obesidade Grave";
+          this.classification = "Invalid"
         }
       },
       clear() {
@@ -65,27 +84,79 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap');
+
+body {
+  background-color: #C5E4E7;
+  font-family: 'Open Sans', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+h1 {
+  text-align: center;
+  color: #00474B;
+  margin: 60px;
+}
+
+.main-content {
+  background-color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 260px;
+  border-radius: 10px;
+}
+
+.calculator-section {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  margin-right: 30px;
 }
 
 .div-imc {
   margin-top: 2rem;
 }
 
-.label-result{
-  font-size: 2rem;
-}
-
-.label-classification {
-  font-size: 1.5rem;
-}
-
 .btn-calculate {
-  margin-right: 2rem !important;
+   width: 230px;
 }
 
+Button {
+  background-color: #1fb19d !important;
+  border-color: #1fb19d !important;
+}
+
+.result-section {
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center; */
+  background-color: #00474B;
+  color: #FFFFFF;
+  width: 250px;
+  height: 220px;
+  border-radius: 5px;
+  opacity: 80%;
+}
+
+.p-title {
+  text-align: center;
+}
+
+.results {
+  padding-left: 15px;
+}
+
+.p-text {
+  margin-top: 1px;
+  margin-bottom: 3px;
+}
+
+.label-result {
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-size: 1.7rem;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import PrimeVue from "primevue/config";
 import Button from  "primevue/button";
 import InputText from "primevue/inputtext";
+import InputMask from 'primevue/inputmask';
 
 import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css"
@@ -12,6 +13,7 @@ const app = createApp(App);
 
 app.use(PrimeVue);
 app.component("Button", Button);
-app.component("InputText", InputText)
+app.component("InputText", InputText);
+app.component("InputMask", InputMask);
 
 app.mount('#app')
